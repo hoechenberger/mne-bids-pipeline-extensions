b33d9b95937fa94df5585e4c47fed80e6348c24a
Richard Höchenberger <richard.hoechenberger@gmail.com>
2023-03-22 09:25:00 +0100

    Initial checkin

---
+++ README.md
@@ -0,0 +1,5 @@
+# MNE-BIDS-Pipeline extension scripts
+
+This repository contains analysis, processing, and visualization scripts that
+ingest MNE-BIDS-Pipeline output. The functionality implemented here is either
+not suitable for or simply has not been integrated into MNE-BIDS-Pipeline yet.
---
+++ source_contrasts/source_contrasts.py
@@ -0,0 +1,391 @@
+"""Produce power contrasts at the source level for custom frequency bands.
+
+- Apply the inverse operator to the covariances of each condition. This yields
+  a source estimate of the power.
+- Calculate the difference of the log10 powers of two conditions (i.e., form a
+  contrast). This yields a ratio of the log10 power: log10(cond_1 / cond_2).
+- Average those contrasts across subjects, yielding a grand average power
+  contrast at the source level.
+
+This was inspired from:
+https://mne.tools/stable/auto_examples/inverse/mne_cov_power.html
+
+© Richard Höchenberger and Charbel-Raphael Segerie.
+
+
+USAGE:
+
+- Run MNE-BIDS-Pipeline completely, including the source estimation steps.
+- Adjust the configuration values below
+  (after "CUSTOM CONFIGURATION STARTS HERE").
+- Run this script via: python source_contrasts.py
+
+
+OUTPUT:
+
+The generated output (STCs and PNG screenshots) will be placed in the
+derivatives folder in a "source_contrasts" sub-directory for each subject.
+Grand-average contrasts will be stored under the "sub-average" subject.
+"""
+
+import os
+from dataclasses import dataclass
+from pathlib import Path
+
+import numpy as np
+
+import mne
+from mne import read_source_estimate
+from mne.epochs import read_epochs
+from mne.minimum_norm.inverse import apply_inverse_cov
+from mne.minimum_norm import read_inverse_operator
+from mne_bids import BIDSPath
+from mne.source_estimate import SourceEstimate
+from mne.utils import logger
+
+
+mne.set_log_level("warning")
+
+
+@dataclass
+class FreqBand:
+    name: str
+    fmin: float | None
+    fmax: float | None
+
+
+@dataclass
+class Contrast:
+    cond_1: str
+    cond_2: str
+
+
+### CUSTOM CONFIGURATION STARTS HERE ###
+
+
+# SPECIFY DERIVATIVES FOLDER HERE
+DERIV_DIR = "~/mne_data/derivatives/mne-bids-pipeline/ds000248_no_mri/"
+
+# SPECIFY FREESURFER SUBJECTS DIRECTORY
+FS_SUBJECTS_DIR = "~/mne_data/ds000248/derivatives/freesurfer/subjects/"
+
+# SPECIFY DATA TYPE HERE
+DATA_TYPE = "meg"
+
+# SPECIFY SUBJECTS HERE
+SUBJECTS = [
+    "sub-01",
+]
+
+# SPECIFY SESSION(S) HERE. PASS None IF NO SESSION NAMES ARE SPECIFIED
+SESSIONS = None
+
+# SPECIFY TASK HERE
+TASK = "audiovisual"
+
+# SPECIFY CONDITIONS TO CONTRAST HERE
+CONTRASTS = [
+    Contrast("Visual/Left", "Visual/Right"),
+    Contrast("Visual", "Auditory"),
+]
+
+# SPECIFY FREQUENCY BANDS HERE (in Hz)
+FREQ_BANDS = [
+    # FreqBand(name='alpha', fmin=8, fmax=12.5),
+    FreqBand(name="beta", fmin=12.5, fmax=30),
+    FreqBand(name="gamma", fmin=30, fmax=100),
+]
+
+# SPECIFY TIME RANGE HERE (in s)
+# TMIN = 1
+# TMAX = 3
+
+TMIN = 0
+TMAX = None
+
+# SPECIFY NUMBER OF PARALLEL PROCESSING JOBS
+N_JOBS = 1
+
+
+### CUSTOM CONFIGURATION ENDS HERE ###
+
+
+DERIV_DIR = Path(DERIV_DIR).expanduser()
+FS_SUBJECTS_DIR = Path(FS_SUBJECTS_DIR).expanduser()
+if SESSIONS is None:
+    SESSIONS = [None]  # makes iteration easier
+
+# Remove "sub-" prefix from subject names if present
+SUBJECTS = [s.split("sub-")[1] if s.startswith("sub-") else s for s in SUBJECTS]
+
+# Avoid resource oversubscription
+os.environ["OMP_NUM_THREADS"] = str(N_JOBS)
+os.environ["NUMEXPR_NUM_THREADS"] = str(N_JOBS)
+os.environ["MKL_NUM_THREADS"] = str(N_JOBS)
+
+
+def process_one_subject(
+    subject: str,
+    session: str | None,
+    task: str,
+    datatype: str,
+    deriv_root: Path,
+    contrasts: list[Contrast],
+    freq_bands: list[FreqBand],
+    tmin: float | None,
+    tmax: float | None,
+    fs_subjects_dir: Path,
+) -> None:
+    """Compute the contrast and morph it to the fsaverage brain."""
+    paths = get_paths(
+        subject=subject,
+        session=session,
+        task=task,
+        datatype=datatype,
+        deriv_root=deriv_root,
+    )
+
+    # Read inverse operator
+    inverse_operator = read_inverse_operator(paths["inverse_operator"])
+
+    # Read epochs
+    epochs = read_epochs(paths["epochs"])
+
+    # Create output directory if it doesn't already exist
+    assert isinstance(paths["output_dir"], Path)
+    paths["output_dir"].mkdir(exist_ok=True)
+
+    # Calculate contrasts in each frequency band
+    for contrast in contrasts:
+        for freq_band in freq_bands:
+            logger.info(
+                f"🏃 processing subject: {subject}, contrast: {contrast}, range: {freq_band.fmin}–{freq_band.fmax} Hz"
+            )
+
+            stcs = []
+            for contrast_condition in ("cond_1", "cond_2"):
+                contrast_condition_name = getattr(contrast, contrast_condition)
+
+                # Filter and then crop the epochs
+                epochs_condition = epochs[contrast_condition_name]
+                epochs_condition_filtered = epochs_condition.filter(
+                    freq_band.fmin, freq_band.fmax
+                )
+                epochs_condition_filtered.crop(tmin=tmin, tmax=tmax)
+
+                cov = mne.compute_covariance(epochs_condition_filtered)
+                stc = apply_inverse_cov(
+                    cov=cov,
+                    info=epochs.info,
+                    inverse_operator=inverse_operator,
+                    nave=len(epochs_condition),
+                    method="dSPM",
+                )
+                stc.data = np.log10(stc.data)
+                stcs.append(stc)
+
+            # Get paths again; now that we have contrast and freq_band, we can
+            # retrieve the paths for the STC, too.
+            paths = get_paths(
+                subject=subject,
+                session=session,
+                task=task,
+                datatype=datatype,
+                deriv_root=deriv_root,
+                contrast=contrast,
+                freq_band=freq_band,
+            )
+
+            # Calculate the contrast
+            stc_contrast = stcs[0] - stcs[1]  # Difference of logs -> ratio
+            stc_contrast.save(paths["stc"], overwrite=True)
+
+            # Plot the contrast
+            brain = plot_stc(stc=stc_contrast, fs_subjects_dir=fs_subjects_dir)
+            brain.save_image(paths["stc_screenshot"], mode="rgb")
+            brain.close()
+
+            # Morph the contrast to fsaverage
+            morph = mne.compute_source_morph(
+                stc_contrast,
+                subject_from=f"sub-{subject}",
+                # subject_from=None,  # ONLY FOR TESTING, DO NOT USE IN PRODUCTION
+                subject_to="fsaverage",
+                subjects_dir=fs_subjects_dir,
+            )
+            stc_contrast_morphed: SourceEstimate = morph.apply(stc_contrast)  # type: ignore
+            stc_contrast_morphed.save(paths["stc_morphed"], overwrite=True)
+
+            # Plot the morphed brain
+            brain = plot_stc(stc=stc_contrast_morphed, fs_subjects_dir=fs_subjects_dir)
+            brain.save_image(paths["stc_morphed_screenshot"], mode="rgb")
+            brain.close()
+
+
+def plot_stc(
+    stc: SourceEstimate,
+    fs_subjects_dir: Path,
+) -> mne.viz.Brain:
+    brain = stc.plot(
+        subjects_dir=fs_subjects_dir,
+        hemi="split",
+        size=(1600, 800),
+        colormap="seismic",
+        clim={"kind": "percent", "pos_lims": [20, 50, 80]},
+    )
+    assert isinstance(brain, mne.viz.Brain)
+    return brain
+
+
+def grand_average(
+    subjects: list[str],
+    session: str | None,
+    task: str,
+    datatype: str,
+    deriv_root: Path,
+    contrasts: list[Contrast],
+    freq_bands: list[FreqBand],
+    fs_subjects_dir: Path,
+) -> None:
+    """Calculate the grand average contrasts."""
+
+    assert subjects
+    assert contrasts
+    assert freq_bands
+
+    # Calculate the grand average contrasts for all frequency bands
+    for contrast in contrasts:
+        for freq_band in freq_bands:
+            logger.info(
+                f"🏃 Grand averaging contrast: {contrast}, range: {freq_band.fmin}–{freq_band.fmax} Hz"
+            )
+
+            # Read the morphed contrasts from all subjects
+            stcs_morphed = []
+            for subject in subjects:
+                paths = get_paths(
+                    subject=subject,
+                    session=session,
+                    task=task,
+                    datatype=datatype,
+                    deriv_root=deriv_root,
+                    contrast=contrast,
+                    freq_band=freq_band,
+                )
+                stc_morphed = read_source_estimate(
+                    paths["stc_morphed"], subject=subject
+                )
+                stcs_morphed.append(stc_morphed)
+
+            paths = get_paths(
+                subject="average",
+                session=session,
+                task=task,
+                datatype=datatype,
+                deriv_root=deriv_root,
+                contrast=contrast,
+                freq_band=freq_band,
+            )
+
+            # Now create a grand average contrast. This is a little hacky
+            stc_grand_mean = stc_morphed.copy()
+            stc_grand_mean.data = np.array([stc.data for stc in stcs_morphed]).mean(
+                axis=0
+            )
+            stc_grand_mean.subject = "fsaverage"
+            stc_grand_mean.save(paths["stc"], overwrite=True)
+
+            # Plot the grand average contrast
+            brain = plot_stc(stc=stc_grand_mean, fs_subjects_dir=fs_subjects_dir)
+            brain.save_image(paths["stc_screenshot"], mode="rgb")
+            brain.close()
+
+
+def get_paths(
+    subject: str,
+    session: str | None,
+    task: str,
+    datatype: str,
+    deriv_root: Path,
+    contrast: Contrast | None = None,
+    freq_band: FreqBand | None = None,
+) -> dict[str, BIDSPath | Path | None]:
+    """Generate a dictionary of paths for use during processing & analysis."""
+
+    # Always start with the clean epochs. We could pick any other existing
+    # file, but epochs seem like a safe bet. The goal here is to generate a
+    # BIDSPath that's un-ambiguous, to avoid any complaints by MNE-BIDS.
+    bp_epochs = BIDSPath(
+        subject=subject,
+        session=session,
+        task=task,
+        suffix="epo",
+        processing="clean",
+        extension=".fif",
+        datatype=datatype,
+        root=deriv_root,
+        check=False,
+    )
+    bp_inv_op = bp_epochs.copy().update(processing=None, suffix="inv")
+    output_dir = bp_epochs.fpath.parent / "source_contrasts"
+
+    if contrast is not None and freq_band is not None:
+        contrast_out_fname = f"sub-{subject}"
+        if session is not None:
+            contrast_out_fname += f"_ses-{session}"
+        contrast_out_fname += f'_task-{task}_{contrast.cond_1.replace("/", "")}+{contrast.cond_2.replace("/", "")}_{freq_band.name}'
+        contrast_out_path = output_dir / contrast_out_fname
+
+        stc_path = contrast_out_path
+        stc_screenshot_path = stc_path.with_suffix(".png")
+
+        stc_morphed_path = stc_path.with_stem(stc_path.stem + "_morphed")
+        stc_morphed_screenshot_path = stc_morphed_path.with_suffix(".png")
+    else:
+        stc_path = None
+        stc_screenshot_path = None
+        stc_morphed_path = None
+        stc_morphed_screenshot_path = None
+
+    return {
+        "output_dir": output_dir,
+        "epochs": bp_epochs,
+        "inverse_operator": bp_inv_op,
+        "stc": stc_path,
+        "stc_screenshot": stc_screenshot_path,
+        "stc_morphed": stc_morphed_path,
+        "stc_morphed_screenshot": stc_morphed_screenshot_path,
+    }
+
+
+def main() -> None:
+    for subject in SUBJECTS:
+        for session in SESSIONS:
+            process_one_subject(
+                subject=subject,
+                session=session,
+                task=TASK,
+                datatype=DATA_TYPE,
+                deriv_root=DERIV_DIR,
+                contrasts=CONTRASTS,
+                freq_bands=FREQ_BANDS,
+                tmin=TMIN,
+                tmax=TMAX,
+                fs_subjects_dir=FS_SUBJECTS_DIR,
+            )
+
+    for session in SESSIONS:
+        grand_average(
+            subjects=SUBJECTS,
+            session=session,
+            task=TASK,
+            datatype=DATA_TYPE,
+            deriv_root=DERIV_DIR,
+            contrasts=CONTRASTS,
+            freq_bands=FREQ_BANDS,
+            fs_subjects_dir=FS_SUBJECTS_DIR,
+        )
+
+
+if __name__ == "__main__":
+    main()
